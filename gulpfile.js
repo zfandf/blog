c79e21b6a0923361cc777502de943d3c05ec124f
zhangff <zhangff@m15.cn>
2015-03-26 09:31:19 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('theme_change', function() {
-   return gulp.src('/themes/pacman/_config.yml')
-       .pipe(gulp.dest('/pacman_theme_config.yml'));
+   return gulp.src('/themes/pacman/_config.yml').pipe(gulp.dest('/pacman_theme_config.yml'));
 });
 // 创建任务
 gulp.task('default', function() {
